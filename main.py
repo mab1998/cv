5fbbe58d4e7e22a21d1b13ffa2a4636d88fa76fb
IceMupppet <mabrouk.bennaoui@gmail.com>
2020-08-31 10:37:56 +0100

    edit function

--- main.py
+++ main.py
@@ -43,37 +43,81 @@ def create_connection(db_file):
 
     return connection
 
-def select_all_tasks(conn,experience,skills,qualification,experience_weight,skills_weight,qualification_weight):
+def select_all_tasks(conn,keywords,experience_weight,skills_weight,qualification_weight,education_weight):
     """
     Query all rows in the tasks table
     :param conn: the Connection object
     :return:
     """
     cur = conn.cursor()
-    experience=""
-    qualification=""
-    skills=""
-    cur.execute(    """SELECT *,
-        IF(
-                `experience` LIKE "{0}%",  {3}, 
-            IF(`experience` LIKE "%{0}%", {3}, 0)
+    # experience=""
+    # qualification=""
+    # keywords=["pipe filter","pipe-filter","pipefilter","plumber","plumbing"]
+    # keywords=["bricklayer",  "brick layer", "brick-layer"]
+
+            
+            
+            
+    rlist={}
+
+    for keyword in keywords:
+        
+        print(cur.execute(    """SELECT qualification_id,
+            IF(
+                    `experience` LIKE "{0}%",  {1}, 
+                IF(`experience` LIKE "%{0}%", {1}, 0)
+            )
+            + IF(`skills` LIKE "%{0}%", {2},  0)
+            + IF(`qualification`         LIKE "%{0}%", {3},  0)
+            + IF(`education`         LIKE "%{0}%", {4},  0)
+            AS `weight`
+        FROM `qualifications`
+        WHERE (
+            `experience` LIKE "%{0}%" 
+            OR `skills` LIKE "%{0}%"
+            OR `qualification`         LIKE "%{0}%"
+            OR `education`         LIKE "%{0}%"
         )
-        + IF(`skills` LIKE "%{1}%", {4},  0)
-        + IF(`qualification`         LIKE "%{2}%", {5},  0)
-        AS `weight`
-    FROM `qualifications`
-    WHERE (
-        `experience` LIKE "%{0}%" 
-        OR `skills` LIKE "%{1}%"
-        OR `qualification`         LIKE "%{2}%"
-    )
-    ORDER BY `weight` DESC
-    LIMIT 20""".format(experience,skills,qualification,experience_weight,skills_weight,qualification_weight))
+        ORDER BY `weight` DESC
+        LIMIT 100""".format(keyword,experience_weight,skills_weight,qualification_weight,education_weight)))
+        rows = cur.fetchall()
+        for row in rows:
+            try:
+                rlist[row[0]]= rlist[row[0]]+row[1]
+            except:
+                rlist[row[0]]= row[1]
+            
+    result={k: v for k, v in sorted(rlist.items(), key=lambda item: item[1],reverse=True)}
 
+    result=sorted(rlist.items(), key=lambda x: x[1], reverse=True)
 
-    rows = cur.fetchall()
+    list_results=[]
+    emails=[]
+    for res in result:
+        
+        
+        jobseeker={}
 
-    return rows
+        cur.execute(    """SELECT jobseeker_id  FROM `jobseeker_qualifications` WHERE qualification_id=%s""", (int(res[0]),))
+
+        jobseeker_id = cur.fetchall()[0][0]
+        conn.commit()
+
+        cur.execute(    """SELECT name,cv_doc_id,phone,email  FROM `jobseeker` WHERE id=%s""", (int(jobseeker_id),))
+        rows=cur.fetchall()[0]
+        conn.commit()
+
+    
+
+        jobseeker['name'] = rows[0]
+        jobseeker['cv_doc_id'] = rows[1]
+        # jobseeker['phone'] = rows[2]
+        # jobseeker['email'] = rows[3]  
+        jobseeker['Rating'] =res[1] 
+        list_results.append(jobseeker)    
+            
+
+    return list_results
 
 
 
@@ -84,9 +128,9 @@ conn=create_connection("d.db")
 
 
 @app.post("/")
-def Search(experience:str,skills:str,qualification:str,experience_weight: Weight = Weight.twenty,skills_weight: Weight = Weight.five,qualification_weight: Weight = Weight.one):
+def Search(keywords:str,experience_weight: int =20,skills_weight: int=30,qualification_weight: int=40,education_weight:int=10):
     # return {"Weight": experience.value}
-    return select_all_tasks(conn,experience,skills,qualification,experience_weight,skills_weight,qualification_weight)
+    return select_all_tasks(conn,keywords,experience_weight,skills_weight,qualification_weight,education_weight)
 
      
     
